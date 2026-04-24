a2375bed5231afaf7a54aba44a8135d34f2ca27e
Ngai Hau Tung <155887013+MoltenMiner@users.noreply.github.com>
2026-04-24 12:44:35 +0800

    Implement user input for sales target and region
    Added input fields for monthly sales target and region selection.

--- Dashboard_practice.py
+++ Dashboard_practice.py
@@ -2,3 +2,12 @@ import streamlit as st
 
 st.title("Retail Business Dashboard")
 st.header("Manager Input Section")
+st.write("Please enter the monthly sales target and select the region.")
+
+
+monthly_salary = st.number_input("Enter Monthly Sales Target (in USD):", min_value=0, max_value=9999999999, value=0)
+region= st.selectbox("Choose your region:", ["East", "South", "West", "North"])
+
+if st.button("Submit"):
+  st.success("Button clicked!")
+  st.write(f"Your salary is {monthly_salary}. You live in the {region} area.")
