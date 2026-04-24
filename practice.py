cf3887d7fa49fbd71e3aa1ef837e13958c7b8507
Ngai Hau Tung <155887013+MoltenMiner@users.noreply.github.com>
2026-04-24 12:17:50 +0800

    Add age input and formatting to practice.py

--- practice.py
+++ practice.py
@@ -2,3 +2,10 @@ import streamlit as st
 st.title("Hello")
 st.header("Isom 3400")
 st.write("I am studying Isom 3400")
+st.write("**Bold Text** and *Italic Text*")
+
+age = st.number_input("Enter your age:",
+                      min_value=0,
+                      max_value=120,
+                      value=25)  #Default value is 25
+st.write(f"Your age is {age}")
