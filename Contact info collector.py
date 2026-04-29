1f5f88db55d9eb943f4ae60a8c9a525636f2e765
Ngai Hau Tung <155887013+MoltenMiner@users.noreply.github.com>
2026-04-30 00:28:47 +0800

    Update Contact info collector.py

--- Contact info collector.py
+++ Contact info collector.py
@@ -9,29 +9,29 @@ with open ("contacts.csv", "w", newline='') as file:
   writer = csv.DictWriter(file, fieldnames= header)
   writer.writeheader()
   
-  with st.form(key="my_form"):
-  
-    First_name = st.text_input("What is your first name?")
-    Last_name = st.text_input("What is your last name?")
-    Fav_no = int(st.number_input("What is your favourite number?"))
-    button = st.form_submit_button("Register")
-  
-    if button:
-      if First_name.strip() != "" and Last_name.strip() != "":
-        contacts_dict = [{"First name": First_name, 
-                      "Last name": Last_name, 
-                      "Favourite number": Fav_no}]
-        
-        with open ("contacts.csv", "a", newline = '') as file:
-          writer = csv.DictWriter(file, fieldnames= header)
-          writer.writerows(contacts_dict)
-        
-        st.success("Successfully added to the file!")
-  
-        with open ("contacts.csv", "r") as file:
-          writer = csv.DictReader(file)
+with st.form(key="my_form"):
+
+  First_name = st.text_input("What is your first name?")
+  Last_name = st.text_input("What is your last name?")
+  Fav_no = int(st.number_input("What is your favourite number?"))
+  button = st.form_submit_button("Register")
+
+  if button:
+    if First_name.strip() != "" and Last_name.strip() != "":
+      contacts_dict = [{"First name": First_name, 
+                    "Last name": Last_name, 
+                    "Favourite number": Fav_no}]
       
-          st.dataframe(writer)
+      with open ("contacts.csv", "a", newline = '') as file:
+        writer = csv.DictWriter(file, fieldnames= header)
+        writer.writerows(contacts_dict)
       
-      elif not First_name or not Last_name or First_name.strip() == "" or Last_name.strip() == "":
-        st.write("You still have not entered all the required items!")
+      st.success("Successfully added to the file!")
+
+      with open ("contacts.csv", "r") as file:
+        writer = csv.DictReader(file)
+    
+        st.dataframe(writer)
+    
+    elif not First_name or not Last_name or First_name.strip() == "" or Last_name.strip() == "":
+      st.write("You still have not entered all the required items!")
