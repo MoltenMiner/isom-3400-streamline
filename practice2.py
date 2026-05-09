e361e1d6e6ec4deb69592478208a8109a9688d0d
Ngai Hau Tung <155887013+MoltenMiner@users.noreply.github.com>
2026-05-09 18:22:22 +0800

    Update practice2.py

--- practice2.py
+++ practice2.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import math
 import pandas as pd
+import time
 from streamlit_option_menu import option_menu
 
 
@@ -70,7 +71,7 @@ else:
 #streamlit_option_menu#
 with st.sidebar:
     selected = option_menu(menu_title="Menu", 
-                            options= ["Home", "About", "Contact", "Data Insights"],
+                            options= ["Home", "About", "Contact", "Data Insights", "Business Perfromance dashboard"],
                             default_index = 0)
 
 if selected == "Home":
@@ -92,8 +93,59 @@ elif selected == "Data Insights":
     st.title(f"Welcome to {selected}!")
     data = st.file_uploader("Upload your csv here", type="csv")
     df = pd.read_csv(data)
-    
+
     st.dataframe(df.head(10))
+#Business dashboard
+elif selected == "Business Performance Dashboard":
+    st.title("Business Performance Dashboard")
+    col1, col2,col3 = st.columns(3)
+
+    with col1:
+        st.header("Q1")
+        st.metrics("$1.2M")
+    with col2:
+        st.header("Q2")
+        st.metrics("$1.5M")
+    with col3:
+        st.header("Q3")
+        st.metrics("$1.3M")
+
+    tab1, tab2, tab3 = st.tabs(["Sales Data", "Customer Insights" , "Market Trends"])
+    with tab1:
+        data = pd.DataFrame({"Quarter": 1, "Revenue": 1.2}, {"Quarter": 2, "Revenue": 1.5},
+                    {"Quarter": 3, "Revenue": 1.3}, {"Quarter": 4, "Revenue": 2})
+        st.dataframe(data)
+    with tab2:
+        comments = ["I love choco!", "I hate choco!", "IDK"]
+        for i in comments:
+            st.write(i)
+
+    with tab3:
+        status = [{"Eco-friendly": "increasing", 
+                   "DEI": "decreasing", 
+                   "Choco": "no change"}]
+        for a,b in status.items:
+            st.write(a, b)
+    with st.expander('More information'):
+        st.write("Data collected from nowhere")
+    box = st.empty()
+    for i in range(5):
+        box.write(f"Loading {i*20}...")
+        time.sleep(1)
+
+    box.write("Business insights: IDK")
+    quarter = st.selectbox("Quarter", ["Q1", "Q2", "Q3", "Q4"])
+    growth = st.slider("Growth", min_value = 0, max_value = 100)
+    rev_map = {"Q1": 1.2, "Q2": 1.5, "Q3": 1.3, "Q4": 1.6}
+    rev = rev_map[quarter]*(1+growth/100)
+    st.write(quarter, rev_map[quarter], rev)
+
+    st.bar_chart(rev_map, x= quarter, y = rev_map.values)
+    
+        
+        
+    
+
 
 
 
